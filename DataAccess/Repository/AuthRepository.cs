89d8ea391ded1ef9f6da80017931519233ea7ca0
Thanh Phuc <thanhphucmp@gmail.com>
2023-05-24 14:58:40 +0700

    upload

---
+++ DataAccess/Repository/AuthRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.DataAcces;
+using System.Linq;
+
+
+namespace DataAccess.Repository
+{
+    public class AuthRepository : IAuthRepository
+    {
+        //new instance of FStoreDBContext, luu trong bo nho
+        // db la tham chieu giu dia chi trong bo nho
+        FStoreContext db = new FStoreContext();
+        public Member checkLogin(string username, string password) 
+            //Linq - ngon ngu truy van tich hop - khi dung truy van du lieu sql - kieu viet khac
+            // FirstOrDefault - extension method (1 trong 5 ae) - di theo Lingq
+            // => - lambda expression (1 trong 5 ae) - di theo Lingq 
+            // nếu chỉ có 1 dòng thay vì dùng dấu {} thì dùng =>
+            => db.Members.FirstOrDefault(x => x.Email.Equals(username) && x.Password.Equals(password));
+    }
+}
---
+++ DataAccess/Repository/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess.DataAcces;
+
+namespace DataAccess.Repository
+{
+    public interface IAuthRepository 
+    {
+        //abstract method (khong dinh nghia body)
+        public Member checkLogin(string username, string password);
+    }
+}
